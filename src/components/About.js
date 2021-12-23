ca7c533330a88751a3b26eb93cb4eaa5584de26e
jennymarie1027 <jennymarie1027@gmail.com>
2021-12-23 12:10:26 -0700

    about me and projects first drafts done

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className='marginTop'>
-            About Me Page
+        <main className='marginTop aboutMeContainer'>
+        {/* <img id="meAndHarry" src="meAndHarry.JPG" alt="me and my cat"> */}
+        <div class="aboutMeDetails">
+            <h2 id="about">ABOUT ME</h2>
+            <p>Hi there! I'm a web designer that specializes in front-end web development.</p>
+            <p id="rectime">In my free time, I'm a dedicated cat mom that enjoys yoga, hiking, and riding bikes with
+                friends. I'm a big fan of coffee, cooking delicious healthy food, and love to travel whenever I can.</p>
         </div>
+        </main>
     )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,59 @@
 import React from 'react'
+import faceDetect from '../imgs/facedetect.png'
+import milk from '../imgs/milk.png'
+import dad from '../imgs/dadjoke.png'
+import { Link } from 'react-router-dom'
 
 const Projects = () => {
     return (
-        <div className='marginTop'>
-            My Projects Page
-        </div>
+        <main className='marginTop'>
+            <section id="projectHeader">
+                <h1>PORTFOLIO</h1>
+                <p>Below you can see some projects I've been working on lately.</p>
+            </section>
+            <section className="project">
+                <h2>Face Censor</h2>
+                <hr />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel officiis dicta iure, reiciendis fuga
+                    commodi
+                    obcaecati debitis dolore exercitationem veritatis, maxime a inventore quibusdam eligendi aperiam
+                    sint.
+                    Quis,
+                    reiciendis eveniet.</p>
+                <div>
+                    <a href='http://wwww.github.com'>Learn More</a>
+                    <a href='http://wwww.github.com'>See it online</a>
+                </div>
+                <img src={faceDetect} alt="censorsed face" />
+            </section>
+            <section class="project">
+                <h2>Candy Museum Site</h2>
+                <hr />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel officiis dicta iure, reiciendis fuga
+                    commodi
+                    obcaecati debitis dolore exercitationem veritatis, maxime a inventore quibusdam eligendi aperiam
+                    sint.
+                    Quis,
+                    reiciendis eveniet.</p>
+                <a href='http://wwww.github.com'>Learn More</a>
+                <a href='http://wwww.github.com'>See it online</a>
+                <img src={milk} alt="candystore logo" />
+                </section>
+            <section class="project">
+                <h2>Dad Joke Generator</h2>
+                <hr />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel officiis dicta iure, reiciendis fuga
+                    commodi
+                    obcaecati debitis dolore exercitationem veritatis, maxime a inventore quibusdam eligendi aperiam sint.
+                    Quis,
+                    reiciendis eveniet.</p>
+                <a href='http://wwww.github.com'>Learn More</a>
+                <a href='http://wwww.github.com'>See it online</a>
+                <div>
+                <img src={dad} alt="dad joke" />
+                </div>
+            </section> 
+        </main>
     )
 }
 
