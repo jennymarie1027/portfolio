78626992c3003f2cae46f3a8b1e9ec2fa6fb22eb
jennymarie1027 <jennymarie1027@gmail.com>
2021-12-27 12:24:07 -0700

    updated projects in portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import faceDetect from '../imgs/facedetect.png'
 import milk from '../imgs/milk.png'
 import dad from '../imgs/dadjoke.png'
@@ -11,45 +12,37 @@ const Projects = () => {
                 <p>Below you can see some projects I've been working on lately.</p>
             </section>
             <section className="project">
-                <h2>Face Censor</h2>
-                <hr />
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel officiis dicta iure, reiciendis fuga
-                    commodi
-                    obcaecati debitis dolore exercitationem veritatis, maxime a inventore quibusdam eligendi aperiam
-                    sint.
-                    Quis,
-                    reiciendis eveniet.</p>
+                <h2>E-Commerce Website</h2>
+                <p>Built an e-commerce website where a user can browse and purchase items with or without an account.  The online store has a complete checkout experience, and user based priviledges so only admin level users are able to manage products.</p>
                 <div>
-                    <a href='http://wwww.github.com'>Learn More</a>
-                    <a href='http://wwww.github.com'>See it online</a>
+                    <a target='_blank' rel="noreferrer noopener" href='https://github.com/2107-CSU/CoolCoders'>Learn More</a>
+                    <a target='_blank' rel="noreferrer noopener"  href='https://jennystrangersthings.netlify.app'>See it online</a>
                 </div>
-                <img src={faceDetect} alt="censorsed face" />
+                {/* <img src={faceDetect} alt="censorsed face" /> */}
             </section>
-            <section class="project">
-                <h2>Candy Museum Site</h2>
-                <hr />
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel officiis dicta iure, reiciendis fuga
-                    commodi
-                    obcaecati debitis dolore exercitationem veritatis, maxime a inventore quibusdam eligendi aperiam
-                    sint.
-                    Quis,
-                    reiciendis eveniet.</p>
-                <a href='http://wwww.github.com'>Learn More</a>
-                <a href='http://wwww.github.com'>See it online</a>
-                <img src={milk} alt="candystore logo" />
-                </section>
-            <section class="project">
-                <h2>Dad Joke Generator</h2>
-                <hr />
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel officiis dicta iure, reiciendis fuga
-                    commodi
-                    obcaecati debitis dolore exercitationem veritatis, maxime a inventore quibusdam eligendi aperiam sint.
-                    Quis,
-                    reiciendis eveniet.</p>
-                <a href='http://wwww.github.com'>Learn More</a>
-                <a href='http://wwww.github.com'>See it online</a>
+            <section className="project">
+                <h2>Stranger's Things</h2>
+                <p>A Craigslist-lite application where users can buy and sell goods online.  Visitors are able to make accounts, create new postings, edit or delete previous posts, and message other users.</p>
                 <div>
-                <img src={dad} alt="dad joke" />
+                    <a target='_blank' rel="noreferrer noopener" href='https://github.com/jennymarie1027/strangers-things-2'>Learn More</a>
+                    <a target='_blank' rel="noreferrer noopener"  href='https://jennystrangersthings.netlify.app'>See it online</a>
+                </div>
+                {/* <img src={faceDetect} alt="censorsed face" /> */}
+            </section>
+            <section className="project">
+                <h2>Fitness Tracker</h2>
+                <p>Built an express server, database adapters and an API using Test-Driven Development.  Also developed the front-end for this application, where users are able to create accounts and develop workout routines for themselves and to share with others.</p>
+                <a target='_blank' rel="noreferrer noopener" href='https://github.com/jennymarie1027/UNIV_FitnessTrackr_Starter'>Learn More</a>
+                <a target='_blank' rel="noreferrer noopener"  href='https://jenny-fitness-tracker.netlify.app/'>See it online</a>
+                {/* <img src={milk} alt="candystore logo" /> */}
+                </section>
+            <section className="project">
+                <h2>Tic Tac Toe</h2>
+                <p>Developed a tic-tac-toe game using vanilla JavaScrip, with the option to play single or dual player.</p>
+                <a target='_blank' rel="noreferrer noopener" href='https://github.com/jennymarie1027/tic-tac-toe'>Learn More</a>
+                <a target='_blank' rel="noreferrer noopener"  href='https://jennystictactoe.netlify.app/'>See it online</a>
+                <div>
+                {/* <img src={dad} alt="dad joke" /> */}
                 </div>
             </section> 
         </main>
