b4511e680a173012b1435662a362b343f1a7e0ca
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-04 11:31:05 -0700

    added new headshot to about.js, added responsive styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import Headshot from '../imgs/headshot2.gif'
 
 const About = () => {
     return (
         <main className='marginTop aboutMeContainer'>
-        {/* <img id="meAndHarry" src="meAndHarry.JPG" alt="me and my cat"> */}
-        <div class="aboutMeDetails">
-            <h2 id="about">ABOUT ME</h2>
-            <p>Hi there! I'm a web designer that specializes in front-end web development.</p>
-            <p id="rectime">In my free time, I'm a dedicated cat mom that enjoys yoga, hiking, and riding bikes with
-                friends. I'm a big fan of coffee, cooking delicious healthy food, and love to travel whenever I can.</p>
-        </div>
+            <div class="aboutMeDetails">
+                <h2 id="about">ABOUT ME</h2>
+                <p>Hi there! I'm a web designer that specializes in front-end web development.</p>
+                <p>In my free time, I'm a dedicated cat mom that enjoys yoga, hiking, and riding bikes with
+                    friends. I'm a big fan of coffee, cooking delicious healthy food, and love to travel whenever I can.</p>
+            </div>
+            <div class='aboutMeHeadshot'>
+                <img id='headshot' src={Headshot} alt='JM Headshot' />
+            </div>
         </main>
     )
 }
