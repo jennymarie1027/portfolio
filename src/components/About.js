d24ff3acfd66565ac67fa0136b3751c1978e147b
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-05 12:01:37 -0700

    updated about paragraph details & homepage sizing

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Headshot from '../imgs/headshot2.gif'
+import {Link} from 'react-router-dom'
 
 const About = () => {
     return (
         <main className='marginTop aboutMeContainer'>
             <div class="aboutMeDetails">
-                <h2 id="about">ABOUT ME</h2>
-                <p>Hi there! I'm a recent graduate from CSU's web development bootcamp that specializes in frontend development.</p>
-                <p>In my free time, I'm a dedicated cat mom that enjoys yoga, hiking, and riding bikes with
-                    friends. I'm a big fan of coffee, cooking delicious, healthy food, and I love to travel whenever I can.</p>
+                <h2 id="about">I'm a curiosity-driven Front-End Developer.</h2>
+                <p></p>
+                <p>I'm passionate about bringing both the technical and visual aspects of digital products to life.  User experience, beautiful pixels and writing clean accessible, human code matters to me.  I sweat the details.  And as a follower of John Maeda's Laws Of Simplicity, I agree that less is more</p>
+                <p>I have a BA in International Affairs & Spanish Literature from the University of Colorado and a Certificate of Web Development from Colorado State University.</p>
+                <p>I am happiest when I'm creating, learning, exploring, and thinking about how to make things better.  In my free time, I enjoy yoga, hiking, and riding bikes with friends. I'm a big fan of coffee, cooking delicious, healthy food, and I love to travel whenever I can.  Currently solving deceptively simple problems at AlgoExpert.com.  I am available for work, feel free to reach out.</p>
+
             </div>
             <div class='aboutMeHeadshot'>
                 <img id='headshot' src={Headshot} alt='JM Headshot' />
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 const Homepage = () => {
     return (
         <main className='marginTop homepageMain'>
-            <p id="hello">Hi, my name is</p>
+            <h5 id="hello">Hi, my name is</h5>
             <h1 id="name">JENNY MCGRATH</h1>
-            <h2 id='subtitle'>I build things for the web.</h2>
-            <p className="intro">I'm a Colorado based programmer that specializes in building (and occasionally designing) exceptional digital experiences. I have a passion for data analytics and creating intuitive user experiences.  </p>
-            <p className="intro">I am a proven leader and life-long learner who is enthusiasic about harnessing technology to solve problems - big and small.  Ready to tackle complex issues with patience and empathy.</p>
+            <h2 className='subtitle'>I build things for the web.</h2>
+            <h4 className="subtitle">I'm a Colorado based programmer with a passion for crafting solid and scalable frontend products with great user experiences.  </h4>
+
 
             <Link className="portfoliobutton" to="/projects">See My Work</Link>
         </main>
