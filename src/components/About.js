6e1cbe00a2164005f219a7b673749a6f4a33abdc
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-05 13:37:23 -0700

    updated homepage btn

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Headshot from '../imgs/headshot2.gif'
-import {Link, Routes} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const About = () => {
     return (
@@ -8,9 +8,9 @@ const About = () => {
             <div className='aboutMeContainer'>
                 <div class="aboutMeDetails">
                     <h2 id="about">I'm a curiosity-driven Front-End Developer</h2>
-                    <p>that is passionate about bringing both the technical and visual aspects of digital products to life.  User experience, beautiful pixels and writing clean accessible, human code matters to me.  I sweat the details.  And as a follower of John Maeda's Laws Of Simplicity, I agree that less is more</p>
+                    <p>that is passionate about bringing both the technical and visual aspects of digital products to life.  User experience, beautiful pixels and writing clean accessible, human code matters to me.  I sweat the details.</p>
                     <p>I have a BA in International Affairs & Spanish Literature from the University of Colorado and a Certificate of Web Development from Colorado State University.</p>
-                    <p>I am happiest when I'm creating, learning, exploring, and thinking about how to make things better.  In my free time, I enjoy yoga, hiking, and riding bikes with friends. I'm a big fan of coffee, cooking delicious, healthy food, and I love to travel whenever I can.  I am available for work, feel free to reach out.</p>
+                    <p>I am happiest when I'm creating, learning, exploring, and thinking about how to make things better.  I'm a big fan of coffee, cooking delicious, healthy food, and I love to travel whenever I can.  I am available for work, feel free to reach out.</p>
                     <Link to='/contact' className='getInTouch'>Get In Touch!</Link>
                 </div>
                 <div class='aboutMeHeadshot'>
