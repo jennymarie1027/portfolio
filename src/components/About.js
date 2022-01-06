f199a61126ded91aff58a3b1ac553e2b44e256ed
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-06 10:44:51 -0700

    changed some wording in about.js and styled title to be inline with paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ const About = () => {
         <main className='marginTop grandpa'>
             <div className='aboutMeContainer'>
                 <div class="aboutMeDetails">
-                    <h2 id="about">I'm a curiosity-driven Front-End Developer</h2>
-                    <p>that is passionate about bringing both the technical and visual aspects of digital products to life.  User experience, beautiful pixels and writing clean accessible, human code matters to me.  I sweat the details.</p>
+                    {/* <h2 id="about">I'm a curiosity-driven Front-End Developer</h2> */}
+                    <p><span id="about">I'm a curiosity-driven Front-End Developer</span> that is passionate about bringing both the technical and visual aspects of digital products to life.  User experience, beautiful design and writing clean, accessible code matters to me.  I sweat the details.</p>
                     <p>I have a BA in International Affairs & Spanish Literature from the University of Colorado and a Certificate of Web Development from Colorado State University.</p>
-                    <p>I am happiest when I'm creating, learning, exploring, and thinking about how to make things better.  I'm a big fan of coffee, cooking delicious, healthy food, and I love to travel whenever I can.  I am available for work, feel free to reach out.</p>
+                    <p>I am happiest when I'm creating, learning and exploring.  I'm a big fan of coffee, cooking delicious, healthy food, and I love to travel whenever I can.  I am available for employment, feel free to reach out.</p>
                     <Link to='/contact' className='getInTouch'>Get In Touch!</Link>
                 </div>
                 <div class='aboutMeHeadshot'>
@@ -39,6 +39,18 @@ const About = () => {
                     <div className='singleSkill'>
                         <i class="fab fa-react"></i>
                         <p>React</p>
+                    </div>
+                    <div className='singleSkill'>
+                        <i class="fab fa-node-js"></i>
+                        <p>Node.js</p>
+                    </div>
+                    <div className='singleSkill'>
+                        <i class="fas fa-server"></i>
+                        <p>Express</p>
+                    </div>
+                    <div className='singleSkill'>
+                        <i class="fas fa-database"></i>
+                        <p>PostgreSQL</p>
                     </div>
                     <div className='singleSkill'>
                         <i class="fab fa-git-alt"></i>
@@ -70,18 +82,6 @@ const About = () => {
                         <i class="fas fa-universal-access"></i>
                         <p className='twoWorkSkill'>Web</p>
                         <p className='twoWorkSkill'>Accessibility</p>
-                    </div>
-                    <div className='singleSkill'>
-                        <i class="fab fa-node-js"></i>
-                        <p>Node.js</p>
-                    </div>
-                    <div className='singleSkill'>
-                        <i class="fas fa-server"></i>
-                        <p>Express</p>
-                    </div>
-                    <div className='singleSkill'>
-                        <i class="fas fa-database"></i>
-                        <p>PostgreSQL</p>
                     </div>
                 </div>
                 <h2 className='skillsTitle'>Currently Working On:</h2>
